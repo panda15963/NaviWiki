a8298569279398273699740949e9742949c3bdce
내비평가스크립트운영실/최민석 <9406979@ict-companion.com>
2023-05-17 15:11:46 +0900

    main

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,11 @@
-
+import NavBar from './component/navbar'
 export default function Home() {
   return (
     <>
+      <div className='container'>
+        <NavBar />
+        <div className='others'>others</div>
+      </div>      
     </>
   )
 }
