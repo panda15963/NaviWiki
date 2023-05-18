37eeb3bb1bee54cfa7b0a3935ff97aea419b9e0e
Minseok Choi <seok7521@gmail.com>
2023-05-18 18:56:25 +0900

    main

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,10 +2,7 @@ import NavBar from './component/navbar'
 export default function Home() {
   return (
     <>
-      <div className='container'>
-        <NavBar />
-        <div className='others'>others</div>
-      </div>      
+      <NavBar />   
     </>
   )
 }
