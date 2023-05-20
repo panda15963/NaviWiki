6d943bff8e8b9190dd338fe6d5fac0509b652f2a
Minseok Choi <seok7521@gmail.com>
2023-05-20 22:04:59 +0900

    main

--- frontend/pages/component/navbar.tsx
+++ frontend/pages/component/navbar.tsx
@@ -1,5 +1,87 @@
-import "tailwindcss"
+import Head from 'next/head'
+import {
+  BsPlus,
+  BsSearch,
+  BsEyeFill,
+  BsBookmarkFill,
+  BsFillArrowLeftSquareFill,
+  BsPeopleFill,
+  BsTerminalFill,
+  BsFillArrowRightSquareFill
+} from 'react-icons/bs'
+import { AiFillFire, AiFillMessage, } from 'react-icons/ai'
+import { IoMdArrowRoundUp } from 'react-icons/io'
+import { MdNightlightRound, MdFeedback } from 'react-icons/md'
+import { FaCog } from 'react-icons/fa'
 export default function NavBar() {
+  const data = [
+    {
+      name: 'Discover',
+      items: [
+        {
+          title: 'Popular',
+          icon: AiFillFire,
+        },
+        {
+          title: 'Most Upvoted',
+          icon: IoMdArrowRoundUp,
+        },
+        {
+          title: 'Best Discussions',
+          icon: AiFillMessage,
+        },
+        {
+          title: 'Search',
+          icon: BsSearch,
+        },
+      ]
+    },
+    {
+      name: 'Manage',
+      items: [
+        {
+          title: 'Bookmarks',
+          icon: BsBookmarkFill,
+        },
+        {
+          title: 'Reading history',
+          icon: BsEyeFill,
+        },
+        {
+          title: 'Focus Mode',
+          icon: MdNightlightRound,
+        },
+        {
+          title: 'Customize',
+          icon: FaCog,
+        },
+      ]
+    },
+  ]  
+  const datafooter = [
+  
+    {
+      name: '',
+      items: [
+        {
+          title: 'Docs',
+          icon: BsBookmarkFill,
+        },
+        {
+          title: 'Changelog',
+          icon: BsTerminalFill,
+        },
+        {
+          title: 'Feedback',
+          icon: MdFeedback,
+        },
+        {
+          title: 'Invite people',
+          icon: BsPeopleFill,
+        },
+      ]
+    },
+  ]
   return (
     <>
         
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {      
+    extend: {   
+      gridTemplateColumns: {
+        sidebar: "300px auto", // 👈 for sidebar layout. adds grid-cols-sidebar class
+      }, 
+      gridTemplateRows: {
+        header: "64px auto", // 👈 for the navbar layout. adds grid-rows-header class
+      },   
     },
   },
   plugins: [],
