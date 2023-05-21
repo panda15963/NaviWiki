a4e984af01761d9e6251bb48dadffd58931c7615
Minseok Choi <seok7521@gmail.com>
2023-05-21 21:15:12 +0900

    main

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,9 +9,11 @@ module.exports = {
     extend: {   
       gridTemplateColumns: {
         sidebar: "300px auto", // 👈 for sidebar layout. adds grid-cols-sidebar class
+        content: "auto", //  for content layout. adds grid-cols-content class
       }, 
       gridTemplateRows: {
         header: "64px auto", // 👈 for the navbar layout. adds grid-rows-header class
+        content: "auto", //  for content layout. adds grid-rows-content class
       },   
     },
   },
