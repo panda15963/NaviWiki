8fb312122df325113f8f4f6f406d0d79b883fb0e
Minseok Choi <seok7521@gmail.com>
2023-05-21 20:57:57 +0900

    main

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,10 @@
-import '@/styles/globals.css'
+import './styles/globals.css'
 import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Layouts from './component/layouts'
+export default function App({ Component, pageProps }: AppProps) {  
+  return (
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+  )
 }
