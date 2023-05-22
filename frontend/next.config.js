5b0d72edf9c4ba9a6f3efa64e03d80a04f70c822
Minseok Choi <seok7521@gmail.com>
2023-05-22 21:49:32 +0900

    main

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0', // default: localhost,
+    timing: false
+  }
 }
 
 module.exports = nextConfig
