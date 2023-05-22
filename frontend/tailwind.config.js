305c0862fc911e97a380cdedf480d5fcb836f75b
Minseok Choi <seok7521@gmail.com>
2023-05-22 21:21:24 +0900

    main

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {   
-      gridTemplateColumns: {
-        sidebar: "300px auto", // 👈 for sidebar layout. adds grid-cols-sidebar class
-        content: "auto", //  for content layout. adds grid-cols-content class
-      }, 
-      gridTemplateRows: {
-        header: "64px auto", // 👈 for the navbar layout. adds grid-rows-header class
-        content: "auto", //  for content layout. adds grid-rows-content class
-      },   
+    extend: {
+      
     },
   },
   plugins: [],
