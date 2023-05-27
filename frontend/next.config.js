0bc332fdcd120620069085107f04c34ffaa6e6f6
Minseok Choi <seok7521@gmail.com>
2023-05-27 16:40:24 +0900

    main

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    disableStaticImages: true,
-  },
 }
 const withImages = require('next-images')
 module.exports = withImages();
